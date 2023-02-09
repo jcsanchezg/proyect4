b420bb6c6a79264aa31ce6cb34baab7c28335428
Csanchez <jcsneo@yahoo.es>
2023-02-08 20:38:51 -0500

    initial code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import HeaderMenu from "./components/HeaderMenu";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <header>{HeaderMenu}</header>
+      <main>
+        soy el menu
+        <section>menu</section>
+        <seccion>blog</seccion>
+      </main>
+      <footer>soy el footer</footer>
+    </React.Fragment>
   );
 }
 
